9addad9915df421a2aa8b80601406db6701de8c6
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-20 23:10:00 -0400

    Added Confirmation Modal

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -6,17 +6,20 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { Notes } from '../api/notes';
 import PropTypes from 'prop-types';
 import { browserHistory } from 'react-router'
+import Modal from 'react-modal'
 // Create editor class
 export class Editor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             title: '',
-            body: ''
+            body: '',
+            modalIsOpen: false
         }
         this.handleBodyChange = this.handleBodyChange.bind(this);
         this.handleTitleChange = this.handleTitleChange.bind(this);
-        this.onDeleteNote = this.onDeleteNote.bind(this)
+        this.onDeleteNote = this.onDeleteNote.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this)
     }
     handleBodyChange(e) {
         const body = e.target.value
@@ -39,28 +42,54 @@ export class Editor extends React.Component {
             });
         };
     }
+    handleModalClose() {
+        this.setState({ modalIsOpen: false })
+    }
     onDeleteNote() {
         this.props.call('notes.remove', this.props.note._id)
         this.props.browserHistory.push("/dashboard")
+        this.handleModalClose()
     }
     render() {
         if (this.props.note) {
             return (
                 <div className="editor">
-                    <input 
+                    <Modal
+                        isOpen={this.state.modalIsOpen}
+                        contentLabel="Add Link"
+                        ariaHideApp={false}
+                        onRequestClose={this.handleModalClose}
+                        shouldCloseOnEsc={true}
+                        shouldCloseOnOverlayClick={true}
+                        className="boxed-view__box"
+                        overlayClassName="boxed-view boxed-view--modal"
+                    >
+                        <h1>Confirm</h1>
+                        <p>Are you sure that you want to delete</p>
+                        <form onSubmit={(e) => e.preventDefault()}className="boxed-view__form">
+                            <button className="button button-danger" onClick={this.onDeleteNote}>Delete</button>
+                            <br/>
+                            <button className="button button--secondary" onClick={this.handleModalClose} type="button ">Cancel</button>
+                        </form>
+                    </Modal>
+                    <input
                         value={this.state.title}
                         placeholder="Untitled Note"
                         onChange={this.handleTitleChange}
                         className="editor__title"
                     />
-                    <textarea 
+                    <textarea
                         value={this.state.body}
                         placeholder="Enter note here..."
                         onChange={this.handleBodyChange}
                         className="editor__body"
                     ></textarea>
                     <div>
-                        <button onClick={this.onDeleteNote}
+                        <button onClick={() => {
+                            this.setState({
+                                modalIsOpen: true
+                            })
+                        }}
                         className="button button-danger">Delete Note</button>
                     </div>
                 </div>
@@ -68,7 +97,7 @@ export class Editor extends React.Component {
         } else {
             return (
                 <div className="editor">
-                    <p className="editor__message">{(this.props.selectedNoteId) ? "Note not found" : "Pick or create a note to get started." }</p>
+                    <p className="editor__message">{(this.props.selectedNoteId) ? "Note not found" : "Pick or create a note to get started."}</p>
                 </div>
             )
         }
